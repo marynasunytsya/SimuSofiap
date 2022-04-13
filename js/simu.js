919aa6abb9d6b18701b894923ecf6795150df64e
Imadeboumediani <boumediani.pro@gmail.com>
2022-04-13 14:09:55 +0200

    changing to  relative path

--- js/simu.js
+++ js/simu.js
@@ -10,14 +10,14 @@ $( document ).ready(function() {
         const page = urlParams.get('page');
 
         var next_page = parseInt(page)+1;
-        document.location.href="http://localhost/simulateur_sofiap/simu.php?page="+next_page;
+        document.location.href="./simu.php?page="+next_page;
         
     });
 
     /*
     function Callpage(page_number, event) {
         event.preventDefault();
-        document.location.href="http://localhost/simulateur_sofiap/simu.php?page="+page_number+1; 
+        document.location.href="./simu.php?page="+page_number+1; 
     }
     */
 });
